55386153a1f6b689c4d5efded25b2eeed2c90f2d
HilinaHilu <166488061+HilinaHilu@users.noreply.github.com>
2025-04-28 14:03:14 +0200

    Add project files.

---
+++ shared/Model/DagligSkæv.cs
@@ -0,0 +1,37 @@
+namespace shared.Model;
+
+public class DagligSkæv : Ordination {
+    public List<Dosis> doser { get; set; } = new List<Dosis>();
+
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel) : base(laegemiddel, startDen, slutDen) {
+	}
+
+    public DagligSkæv(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, Dosis[] doser) : base(laegemiddel, startDen, slutDen) {
+        this.doser = doser.ToList();
+    }    
+
+    public DagligSkæv() : base(null!, new DateTime(), new DateTime()) {
+    }
+
+	public void opretDosis(DateTime tid, double antal) {
+        doser.Add(new Dosis(tid, antal));
+    }
+
+	public override double samletDosis() {
+		return base.antalDage() * doegnDosis();
+	}
+
+	public override double doegnDosis() {
+		// Sum the 'antal' for each dose in the list 'doser'
+		double totalDosis = 0;
+		foreach (var dosis in doser) {
+			totalDosis += dosis.DosisId;  // Assuming 'dosis' holds the amount for each dose
+		}
+		return totalDosis;
+	}
+
+
+	public override String getType() {
+		return "DagligSkæv";
+	}
+}
